75a584a3bfdcfbe31d9c08fcfee58505b2b1db02
Gabriel Bergmann <gabrielbergmann10@hotmail.com>
2020-02-05 17:19:57 -0300

    Mod erro

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var model = require("../models/index");
+var model = require("..//index");
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
